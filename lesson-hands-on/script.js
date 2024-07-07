4b73ebdfaeb01e664c52b6790cbbac1b40e5aa6a
redsareraw <dmarshall@musfiber.net>
2024-07-07 15:56:28 -0400

    I saved my progress on the script.js file.

--- lesson-hands-on/script.js
+++ lesson-hands-on/script.js
@@ -32,3 +32,12 @@ let cars = [
 ]
 let car = cars.find(car => car.color === "red");
 console.log(car);
+
+/* There are seven primitive data types: boolean, number, string, null, undefined and symbol.
+An example of this would be the variable start which is a number, the number 2.
+Some examples of non-primitive data types would be: objects, arrays and functions.
+An example of this would be the array sports[].
+The distinction between the two can be stated by four different situations: immutability versus
+mutability, storage, assignment, and comparision. This is important to know because of how you 
+interact with your data. You may get what you think are erroneous outputs because of the type of
+data you are working with. */
